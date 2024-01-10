898bb96db0d19d522c11b8f5f0128e73392a76ed
arcic12 <2305567@kiit.ac.in>
2024-01-10 22:49:45 +0530

    updated temporary folder

---
+++ temporary/area.java
@@ -0,0 +1,12 @@
+public class area {
+
+    public static void main(String[] args) {
+        
+        int length=5;
+        int breadth=2;
+
+        System.out.println("The area of the rectangle is "+(length*breadth));
+        
+    }
+    
+}
